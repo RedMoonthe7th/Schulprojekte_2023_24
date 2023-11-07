5fbf705fdc35be5d5c6b139c0885c699272d13f2
Alexander II <alexander.tschanun@icloud.com>
2023-11-07 09:09:29 +0100

    07.11.2023_JSÜbung01_finished&&Reorganisation

---
+++ JS-Basics/JS_Übung_01/script.js
@@ -0,0 +1,38 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+//Variables
+let a = 7;
+let b = 8;
+let c = a + b
+
+console.log("A hat den Wert " + a)
+
+a= 10;
+console.log("A hat den Wert " + a)
+
+console.log("a + b" + c)
+
+// Berechne folgendes und gib das Eregebnis über console.log aus 
+
+let result = a + b + c;
+console.log("Das Result ist " + result)
+
+result = (a + b) / c 
+console.log("Das Result ist " + result)
+result = c - a + b 
+console.log("Das Result ist " + result)
+result = a + b + c * 3 
+console.log("Das Result ist " + result)
+result = (a + b + c)/4 
+console.log("Das Result ist " + result)
+ 
+
+// Ändere die Variablen auf folgende Werte 
+// gib die neuen Werte der Variablen aus 
+a = 3 
+console.log("a ist " + a)
+b = a + 3 
+console.log("b ist " + b)
+c = c -3 
+console.log("c ist " + c)
