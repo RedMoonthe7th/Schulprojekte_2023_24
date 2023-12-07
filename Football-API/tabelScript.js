4bb383a619f40e55f2cb0c93440865ee01b8289e
Alexander II <alexander.tschanun@icloud.com>
2023-12-07 13:14:51 +0100

    07.12.2023_Football-API_Progress&&Data

---
+++ Football-API/tabelScript.js
@@ -0,0 +1,25 @@
+alert("Hello")
+console.log("Test")
+
+/*Tabel*/
+fetch("https://api.openligadb.de/getbltable/bl1/2023").then((result) => {
+    console.log("Hat funktioniert Teil 1")
+    result.json().then((data) => {
+        getHTMLForTabel(data);
+        console.log(data)
+    })
+})
+
+function getHTMLForTabel(data){
+    let tabelHtml = "<ul>";
+
+    /* + "<div id=''>" + index + "</div>" */
+    data.forEach((element,index) =>{
+        index += 1;
+        tabelHtml += "<div id='index'>" + index + ".</div>" + "<div id='logo'><img src=" + element.teamIconUrl + "></div>" + "<div id='name'>" + element.teamName + "</div>" + "<div id='matches'>" + element.matches + "</div>" + "<div id='wins'>" + element.won + "</div>"  + "<div id='draws'>" + element.draw + "</div>" + "<div id='losses'>" + element.lost + "</div>" + "<div id='goals'>" + element.goals + ": </div>" + "<div id='enemyGoals'>" + element.opponentGoals + "</div>" + "<div id='difference'>" + element.goalDiff + "</div>"  + "<div id='points'>" + element.points + "</div>";
+    });
+    tabelHtml += "</ul>";
+    document.getElementById("tabelContent").innerHTML = tabelHtml;
+}
+
+/*Clubs*/
