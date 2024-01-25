53998c5cba2f77d39be8b5edda8f5a812f4a5d5a
Alexander II <alexander.tschanun@icloud.com>
2024-01-25 14:44:24 +0100

    25.01.2024_PeopleContainer_done

--- REACT/2024_react/src/components/PeopleContainer.js
+++ REACT/2024_react/src/components/PeopleContainer.js
@@ -3,20 +3,31 @@ import Card from './Card'
 
 export default function PeopleContainer() {
     const [people, setPeople] = useState([]);
+    const [filteredPeople, setFilteredPeople] = useState([]);
 
     useEffect(()=>{
         fetch("https://65a637af74cf4207b4ef7cd2.mockapi.io/redMoon/Person").then(res => res.json().then(data =>{
-            alert("Success");
             setPeople(data);
+            setFilteredPeople(data)
         }))
 
     },[])
 
+    const filterPeople = (filter) =>{
+        let filtered = people.filter(person => person.name.toLowerCase().includes(filter));
+        setFilteredPeople(filtered)
+    }
+
     return (
-        <div>
-            <h1>People</h1>
-            <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
-                {people.map(person => {
+        <div className=''>
+            <div className='fixed border mb-4 bg-white w-full h-28 p-6'>
+            <input className='border p-4' type="text" placeholder='Search' onChange={(el)=>{
+                filterPeople(el.target.value)
+            }} ></input>
+            </div>
+
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 p-8 pt-32'>
+                {filteredPeople.map(person => {
                     return <Card name={person.name} description={person.description} imageUrl={person.avatar}/>
                 })}
             </div>
