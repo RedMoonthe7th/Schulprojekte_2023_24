d75a4b9ada12bf1b48bdd7e93859bf466f2a02e2
Alexander II <alexander.tschanun@icloud.com>
2024-02-23 10:43:01 +0100

    23.02.2024_Football-API_Navigation-Router

--- REACT/2024_react/src/App.js
+++ REACT/2024_react/src/App.js
@@ -1,16 +1,28 @@
 import './index.css';
 import {
   createBrowserRouter,
-  RouterProvider,
+  BrowserRouter as Router,
+  Route,
+  Routes,
 } from "react-router-dom";
 import FootballMenu from './components/Football-API/FootballMenu';
 import FootballHeaderNav from './components/Football-API/FootballHeaderNav';
+import FootballClubs from './components/Football-API/FootballClubs';
 
 function App() {
   return (
-    <div>
-      <FootballHeaderNav/>
-      <FootballMenu/>
+    <div className='bg-[#2B3434]'>
+      <Router>
+      <div><FootballHeaderNav /></div>
+
+       
+        <Routes>
+          <Route path='/' element={<div>Placeholder</div>}/>
+          <Route path='/Gameplan' element={<div>Placeholder</div>}/>
+          <Route path="/Table" element={<div>Placeholder</div>}/>
+          <Route path="/Clubs" element={<FootballClubs />}/>
+        </Routes>
+      </Router>
     </div>
 
   );
--- REACT/2024_react/src/components/Football-API/FootballHeaderNav.js
+++ REACT/2024_react/src/components/Football-API/FootballHeaderNav.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
 
 export default function FootballHeaderNav() {
-    const navigate = useNavigate();
     return (
-        <ul class="nav">
-            <img src='src/components/Football-API/bundesliga_pos.png'></img>
-            <li>Home</li>
-            <li>Spielplan</li>     
-            <li>Tabelle</li>
-            <li onClick={() => {navigate("/clubs")}}>Clubs</li>
-        </ul>
+        <div className='flex gap-80 items-center'>
+            <img src="bundesliga_pos.png" alt="bundesliga_pos"></img>
+            <div className='flex gap-32'>
+                <NavLink
+                    to="/"
+                    className={({ isActive }) => isActive ? 'activeLink' : 'navLink'}>
+                    Home
+                </NavLink>
+                <NavLink
+                    to="/Gameplan"
+                    className={({ isActive }) => isActive ? 'activeLink' : 'navLink'}>
+                    Gameplan
+                </NavLink>
+                <NavLink
+                    to="/Table"
+                    className={({ isActive }) => isActive ? 'activeLink' : 'navLink'}>
+                    Table
+                </NavLink>
+                <NavLink
+                    to="/Clubs"
+                    className={({ isActive }) => isActive ? 'activeLink' : 'navLink'}>
+                    Clubs
+                </NavLink>
+            </div>
+        </div>
+
     )
 }
